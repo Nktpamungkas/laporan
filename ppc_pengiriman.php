fc8148b416e02f4bac3fdcc42d268d0971ba3c28
Nilokusumo Tri Pamungkas <44149646+Nktpamungkas@users.noreply.github.com>
2023-06-13 16:30:26 +0700

    format tanggal difield GOODSISSUEDATE

--- ppc_pengiriman.php
+++ ppc_pengiriman.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>PPC - Memo Penting</title>
+    <title>PPC - Laporan Pengiriman</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -40,19 +40,16 @@
                                     <div class="card-block">
                                         <form action="" method="post">
                                             <div class="row">
-                                                <div class="col-sm-12 col-xl-2 m-b-30">
+                                                <div class="col-sm-12 col-xl-6 m-b-30">
                                                     <h4 class="sub-title">Issue Date From</h4>
                                                     <input type="date" name="tgl1" class="form-control" id="tgl1" value="<?php if (isset($_POST['submit'])){ echo $_POST['tgl1']; } ?>">
                                                 </div>
-                                                <div class="col-sm-12 col-xl-2 m-b-30">
+                                                <div class="col-sm-12 col-xl-6 m-b-30">
                                                     <h4 class="sub-title">Until Date</h4>
                                                     <input type="date" name="tgl2" class="form-control" id="tgl2" value="<?php if (isset($_POST['submit'])){ echo $_POST['tgl2']; } ?>">
                                                 </div>
-                                                <div class="col-sm-12 col-xl-4 m-b-30">
+                                                <div class="col-sm-12 col-xl-12 m-b-30">
                                                     <button type="submit" name="submit" class="btn btn-primary">Cari data</button>
-                                                    <?php if (isset($_POST['submit'])) : ?>
-                                                        <a class="btn btn-mat btn-success" href="ppc_memopenting-excel.php?no_order=<?= $_POST['no_order']; ?>&tgl1=<?= $_POST['tgl1']; ?>&tgl2=<?= $_POST['tgl2']; ?>">CETAK EXCEL</a>
-                                                    <?php endif; ?>
                                                 </div>
                                             </div>
                                         </form>
@@ -62,10 +59,24 @@
                                     <div class="card">
                                         <div class="card-block">
                                             <div class="dt-responsive table-responsive">
-                                                <table id="lang-dt" class="table table-striped table-bordered nowrap">
+                                                <table id="excel-LA" class="table table-striped table-bordered nowrap">
                                                     <thead>
                                                         <tr>
-                                                            <th>KETERANGAN</th>
+                                                            <th>NO</th>
+                                                            <th>TANGGAL</th>
+                                                            <th>NO SJ</th>
+                                                            <th>WARNA</th>
+                                                            <th>ROLL</th>
+                                                            <th>QUANTITY</th>
+                                                            <th>BUYER</th>
+                                                            <th>CUSTOMER</th>
+                                                            <th>NO PO</th>
+                                                            <th>NO ORDER</th>
+                                                            <th>JENIS KAIN</th>
+                                                            <th>LOTCODE</th>
+                                                            <th>DEMAND</th>
+                                                            <th>FOC</th>
+                                                            <th>TYPE</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody> 
@@ -77,16 +88,110 @@
                                                             $tgl2     = $_POST['tgl2'];
 
                                                             if($tgl1 & $tgl2){
-                                                                $where_date     = "DELIVERY BETWEEN '$tgl1' AND '$tgl2'";
+                                                                $where_date     = "i.GOODSISSUEDATE BETWEEN '$tgl1' AND '$tgl2'";
                                                             }else{
                                                                 $where_date     = "";
                                                             }
-                                                            $sqlDB2 = "";
+                                                            $sqlDB2 = "SELECT 
+                                                                            i.PROVISIONALCODE,
+                                                                            i.DEFINITIVEDOCUMENTDATE,
+                                                                            i.ORDERPARTNERBRANDCODE,
+                                                                            i.PO_NUMBER,
+                                                                            i.PROJECTCODE,
+                                                                            DAY(i.GOODSISSUEDATE) ||'-'|| MONTHNAME(i.GOODSISSUEDATE) ||'-'|| YEAR(i.GOODSISSUEDATE) AS GOODSISSUEDATE,
+                                                                            i.ORDPRNCUSTOMERSUPPLIERCODE,
+                                                                            i.PAYMENTMETHODCODE,
+                                                                            i.EXTERNALITEMCODE,    
+                                                                            i.ITEMTYPEAFICODE,
+                                                                            i.SUBCODE01,
+                                                                            i.SUBCODE02,
+                                                                            i.SUBCODE03,
+                                                                            i.SUBCODE04,
+                                                                            i.SUBCODE05,
+                                                                            i.SUBCODE06,
+                                                                            i.SUBCODE07,
+                                                                            i.SUBCODE08,
+                                                                            i.DLVSALORDERLINESALESORDERCODE,
+                                                                            i.DLVSALESORDERLINEORDERLINE,
+                                                                            i.ITEMDESCRIPTION,
+                                                                            i.ORDERLINE,
+                                                                            LISTAGG(TRIM(i.LOTCODE), ', ') AS LOTCODE,
+                                                                            LISTAGG(DBL.PRODUCTIONDEMANDCODE, ', ') AS PRODUCTIONDEMANDCODE,
+                                                                            i.CODE,
+                                                                            i2.WARNA
+                                                                        FROM 
+                                                                            ITXVIEWLAPKIRIMPPC i 
+                                                                        LEFT JOIN ITXVIEW_DEMANDBYLOTCODE DBL ON DBL.PRODUCTIONORDERCODE = i.LOTCODE AND DBL.DLVSALESORDERLINEORDERLINE = i.DLVSALESORDERLINEORDERLINE
+                                                                        LEFT JOIN ITXVIEWCOLOR i2 ON i2.ITEMTYPECODE =  i.ITEMTYPEAFICODE
+                                                                                                AND i2.SUBCODE01 = i.SUBCODE01 AND i2.SUBCODE02 = i.SUBCODE02
+                                                                                                AND i2.SUBCODE03 = i.SUBCODE03 AND i2.SUBCODE04 = i.SUBCODE04
+                                                                                                AND i2.SUBCODE05 = i.SUBCODE05 AND i2.SUBCODE06 = i.SUBCODE06
+                                                                                                AND i2.SUBCODE07 = i.SUBCODE07 AND i2.SUBCODE08 = i.SUBCODE08
+                                                                        WHERE 
+                                                                            $where_date
+                                                                        GROUP BY 
+                                                                            i.PROVISIONALCODE,
+                                                                            i.DEFINITIVEDOCUMENTDATE,
+                                                                            i.ORDERPARTNERBRANDCODE,
+                                                                            i.PO_NUMBER,
+                                                                            i.PROJECTCODE,
+                                                                            i.GOODSISSUEDATE,
+                                                                            i.ORDPRNCUSTOMERSUPPLIERCODE,
+                                                                            i.PAYMENTMETHODCODE,
+                                                                            i.EXTERNALITEMCODE,    
+                                                                            i.ITEMTYPEAFICODE,
+                                                                            i.SUBCODE01,
+                                                                            i.SUBCODE02,
+                                                                            i.SUBCODE03,
+                                                                            i.SUBCODE04,
+                                                                            i.SUBCODE05,
+                                                                            i.SUBCODE06,
+                                                                            i.SUBCODE07,
+                                                                            i.SUBCODE08,
+                                                                            i.DLVSALORDERLINESALESORDERCODE,
+                                                                            i.DLVSALESORDERLINEORDERLINE,
+                                                                            i.ITEMDESCRIPTION,
+                                                                            i.ORDERLINE,
+                                                                            i.CODE,
+                                                                            i2.WARNA";
                                                             $stmt   = db2_exec($conn1,$sqlDB2);
+                                                            $no = 1;
                                                             while ($rowdb2 = db2_fetch_assoc($stmt)) {
                                                         ?>
                                                         <tr>
-                                                            <td></td> <!-- TGL TERIMA ORDER -->
+                                                            <td><?= $no++; ?></td>
+                                                            <td><?= $rowdb2['GOODSISSUEDATE']; ?></td> 
+                                                            <td><?= $rowdb2['PROVISIONALCODE']; ?></td> 
+                                                            <td><?= $rowdb2['WARNA']; ?></td> 
+                                                            <td>
+                                                                <?php
+                                                                    $q_roll     = db2_exec($conn1, "SELECT COUNT(CODE) AS ROLL,
+                                                                                                            SUM(BASEPRIMARYQUANTITY) AS QTY_SJ
+                                                                                                    FROM 
+                                                                                                        ITXVIEWALLOCATION0 
+                                                                                                    WHERE 
+                                                                                                        CODE = '$rowdb2[CODE]'");
+                                                                    $d_roll     = db2_fetch_assoc($q_roll);
+                                                                    echo $d_roll['ROLL'];
+                                                                ?>
+                                                            </td> 
+                                                            <td><?= $d_roll['QTY_SJ'] ?></td> 
+                                                            <td><?= $rowdb2['ORDERPARTNERBRANDCODE']; ?></td> 
+                                                            <td>
+                                                                <?php
+                                                                    $q_pelanggan    = db2_exec($conn1, "SELECT * FROM ITXVIEW_PELANGGAN WHERE ORDPRNCUSTOMERSUPPLIERCODE = '$rowdb2[ORDPRNCUSTOMERSUPPLIERCODE]' 
+                                                                                                                                        AND CODE = '$rowdb2[DLVSALORDERLINESALESORDERCODE]'");
+                                                                    $r_pelanggan    = db2_fetch_assoc($q_pelanggan);
+                                                                    echo $r_pelanggan['LANGGANAN'];
+                                                                ?>
+                                                            </td> 
+                                                            <td>`<?= $rowdb2['PO_NUMBER']; ?></td> 
+                                                            <td><?= $rowdb2['DLVSALORDERLINESALESORDERCODE']; ?></td> 
+                                                            <td><?= $rowdb2['ITEMDESCRIPTION']; ?></td> 
+                                                            <td>`<?= $rowdb2['LOTCODE']; ?></td> 
+                                                            <td>`<?= $rowdb2['PRODUCTIONDEMANDCODE']; ?></td> 
+                                                            <td><?php if($rowdb2['PAYMENTMETHODCODE'] == 'FOC'){ echo $rowdb2['PAYMENTMETHODCODE']; } ?></td> 
+                                                            <td><?= $rowdb2['ITEMTYPEAFICODE']; ?></td> 
                                                         </tr>
                                                         <?php } ?>
                                                     </tbody>
@@ -103,4 +208,59 @@
         </div>
     </div>
 </body>
+<script type="text/javascript" src="files\bower_components\jquery\js\jquery.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\jquery-ui\js\jquery-ui.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\popper.js\js\popper.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\bootstrap\js\bootstrap.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\jquery-slimscroll\js\jquery.slimscroll.js"></script>
+    <script type="text/javascript" src="files\bower_components\modernizr\js\modernizr.js"></script>
+    <script type="text/javascript" src="files\bower_components\modernizr\js\css-scrollbars.js"></script>
+    <script src="files\bower_components\datatables.net\js\jquery.dataTables.min.js"></script>
+    <script src="files\bower_components\datatables.net-buttons\js\dataTables.buttons.min.js"></script>
+    <script src="files\assets\pages\data-table\js\jszip.min.js"></script>
+    <script src="files\assets\pages\data-table\js\pdfmake.min.js"></script>
+    <script src="files\assets\pages\data-table\js\vfs_fonts.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\dataTables.buttons.min.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\buttons.flash.min.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\jszip.min.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\vfs_fonts.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\buttons.colVis.min.js"></script>
+    <script src="files\bower_components\datatables.net-buttons\js\buttons.print.min.js"></script>
+    <script src="files\bower_components\datatables.net-buttons\js\buttons.html5.min.js"></script>
+    <script src="files\bower_components\datatables.net-bs4\js\dataTables.bootstrap4.min.js"></script>
+    <script src="files\bower_components\datatables.net-responsive\js\dataTables.responsive.min.js"></script>
+    <script src="files\bower_components\datatables.net-responsive-bs4\js\responsive.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\i18next\js\i18next.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\i18next-xhr-backend\js\i18nextXHRBackend.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\i18next-browser-languagedetector\js\i18nextBrowserLanguageDetector.min.js"></script>
+    <script type="text/javascript" src="files\bower_components\jquery-i18next\js\jquery-i18next.min.js"></script>
+    <script src="files\assets\pages\data-table\extensions\buttons\js\extension-btns-custom.js"></script>
+    <script src="files\assets\js\pcoded.min.js"></script>
+    <script src="files\assets\js\menu\menu-hori-fixed.js"></script>
+    <script src="files\assets\js\jquery.mCustomScrollbar.concat.min.js"></script>
+    <script type="text/javascript" src="files\assets\js\script.js"></script>
+
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23581568-13');
+    </script>
+    <script>
+        $('#excel-LA').DataTable({
+            dom: 'Bfrtip',
+            buttons: [{
+                extend: 'excelHtml5',
+                customize: function(xlsx) {
+                    var sheet = xlsx.xl.worksheets['sheet1.xml'];
+                    $('row c[r^="F"]', sheet).each(function() {
+                        if ($('is t', this).text().replace(/[^\d]/g, '') * 1 >= 500000) {
+                            $(this).attr('s', '20');
+                        }
+                    });
+                }
+            }]
+        });
+    </script>
 <?php require_once 'footer.php'; ?>
