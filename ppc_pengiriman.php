caf86a7c0e998a435b4c5f563d8874208cbe97eb
Nilokusumo Tri Pamungkas <44149646+Nktpamungkas@users.noreply.github.com>
2023-06-13 11:11:05 +0700

    Menu baru laporan pengiriman

---
+++ ppc_pengiriman.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PPC - Memo Penting</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="#">
+    <meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="#">
+    <link rel="icon" href="files\assets\images\favicon.ico" type="image/x-icon">
+     <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet"> -->
+    <link rel="stylesheet" type="text/css" href="files\bower_components\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\icon\themify-icons\themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\icon\icofont\css\icofont.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\icon\feather\css\feather.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\pages\prism\prism.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\css\style.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\css\jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\css\pcoded-horizontal.min.css">
+    <link rel="stylesheet" type="text/css" href="files\bower_components\datatables.net-bs4\css\dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="files\assets\pages\data-table\css\buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="files\bower_components\datatables.net-responsive-bs4\css\responsive.bootstrap4.min.css">
+    <script src="TabCounter.js"></script>
+</head>
+<?php require_once 'header.php'; ?>
+
+<body>
+    <div class="pcoded-content">
+        <div class="pcoded-inner-content">
+            <div class="main-body">
+                <div class="page-wrapper">
+                    <div class="page-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-block">
+                                        <form action="" method="post">
+                                            <div class="row">
+                                                <div class="col-sm-12 col-xl-2 m-b-30">
+                                                    <h4 class="sub-title">Dari Tanggal</h4>
+                                                    <input type="date" name="tgl1" class="form-control" id="tgl1" value="<?php if (isset($_POST['submit'])){ echo $_POST['tgl1']; } ?>">
+                                                </div>
+                                                <div class="col-sm-12 col-xl-2 m-b-30">
+                                                    <h4 class="sub-title">Sampai Tanggal</h4>
+                                                    <input type="date" name="tgl2" class="form-control" id="tgl2" value="<?php if (isset($_POST['submit'])){ echo $_POST['tgl2']; } ?>">
+                                                </div>
+                                                <div class="col-sm-12 col-xl-4 m-b-30">
+                                                    <button type="submit" name="submit" class="btn btn-primary">Cari data</button>
+                                                    <?php if (isset($_POST['submit'])) : ?>
+                                                        <a class="btn btn-mat btn-success" href="ppc_memopenting-excel.php?no_order=<?= $_POST['no_order']; ?>&tgl1=<?= $_POST['tgl1']; ?>&tgl2=<?= $_POST['tgl2']; ?>">CETAK EXCEL</a>
+                                                    <?php endif; ?>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <?php if (isset($_POST['submit'])) : ?>
+                                    <div class="card">
+                                        <div class="card-block">
+                                            <div class="dt-responsive table-responsive">
+                                                <table id="lang-dt" class="table table-striped table-bordered nowrap">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>KETERANGAN</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody> 
+                                                        <?php 
+                                                            ini_set("error_reporting", 1);
+                                                            session_start();
+                                                            require_once "koneksi.php";
+                                                            $prod_order  = $_POST['prod_order'];
+                                                            $prod_demand = $_POST['prod_demand'];
+                                                            $no_order = $_POST['no_order'];
+                                                            $tgl1     = $_POST['tgl1'];
+                                                            $tgl2     = $_POST['tgl2'];
+
+                                                            if($prod_order){
+                                                                $where_prodorder    = "NO_KK  = '$prod_order'";
+                                                            }else{
+                                                                $where_prodorder    = "";
+                                                            }
+                                                            if($prod_demand){
+                                                                $where_proddemand    = "DEMAND = '$prod_demand'";
+                                                            }else{
+                                                                $where_proddemand    = "";
+                                                            }
+                                                            if($no_order){
+                                                                $where_order    = "NO_ORDER = '$no_order'";
+                                                            }else{
+                                                                $where_order    = "";
+                                                            }
+                                                            if($tgl1 & $tgl2){
+                                                                $where_date     = "DELIVERY BETWEEN '$tgl1' AND '$tgl2'";
+                                                            }else{
+                                                                $where_date     = "";
+                                                            }
+                                                            // itxview_memopentingppc
+                                                            $itxviewmemo              = db2_exec($conn1, "SELECT * FROM itxview_memopentingppc WHERE $where_prodorder $where_proddemand $where_order $where_date");
+                                                            while ($row_itxviewmemo   = db2_fetch_assoc($itxviewmemo)) {
+                                                                $r_itxviewmemo[]      = "('".TRIM(addslashes($row_itxviewmemo['ORDERDATE']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['PELANGGAN']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['NO_ORDER']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['NO_PO']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['KETERANGAN_PRODUCT']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['WARNA']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['NO_WARNA']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['DELIVERY']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['QTY_BAGIKAIN']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['NETTO']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['DELAY']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['NO_KK']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['DEMAND']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['ORDERLINE']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['PROGRESSSTATUS']))."',"
+                                                                                        ."'".TRIM(addslashes($row_itxviewmemo['KETERANGAN']))."',"
+                                                                                        ."'".$_SERVER['REMOTE_ADDR']."',"
+                                                                                        ."'".date('Y-m-d H:i:s')."')";
+                                                            }
+                                                            $value_itxviewmemo        = implode(',', $r_itxviewmemo);
+                                                            $insert_itxviewmemo       = mysqli_query($con_nowprd, "INSERT INTO itxview_memopentingppc(ORDERDATE,PELANGGAN,NO_ORDER,NO_PO,KETERANGAN_PRODUCT,WARNA,NO_WARNA,DELIVERY,QTY_BAGIKAIN,NETTO,`DELAY`,NO_KK,DEMAND,ORDERLINE,PROGRESSSTATUS,KETERANGAN,IPADDRESS,CREATEDATETIME) VALUES $value_itxviewmemo");
+
+                                                            // --------------------------------------------------------------------------------------------------------------- //
+                                                            $prod_order_2  = $_POST['prod_order'];
+                                                            $prod_demand_2 = $_POST['prod_demand'];
+                                                            $no_order_2 = $_POST['no_order'];
+                                                            $tgl1_2     = $_POST['tgl1'];
+                                                            $tgl2_2     = $_POST['tgl2'];
+
+                                                            if($prod_order_2){
+                                                                $where_prodorder2    = "NO_KK  = '$prod_order'";
+                                                            }else{
+                                                                $where_prodorder2    = "";
+                                                            }
+                                                            if($prod_demand_2){
+                                                                $where_proddemand2    = "DEMAND = '$prod_demand'";
+                                                            }else{
+                                                                $where_proddemand2    = "";
+                                                            }
+                                                            if($no_order_2){
+                                                                $where_order2    = "NO_ORDER = '$no_order_2'";
+                                                            }else{
+                                                                $where_order2    = "";
+                                                            }
+                                                            if($tgl1_2 & $tgl2_2){
+                                                                $where_date2     = "DELIVERY BETWEEN '$tgl1_2' AND '$tgl2_2'";
+                                                            }else{
+                                                                $where_date2     = "";
+                                                            }
+                                                            $sqlDB2 = "SELECT DISTINCT * FROM itxview_memopentingppc WHERE $where_prodorder2 $where_proddemand2 $where_order2 $where_date2 AND IPADDRESS = '$_SERVER[REMOTE_ADDR]' ORDER BY DELIVERY ASC";
+                                                            $stmt   = mysqli_query($con_nowprd,$sqlDB2);
+                                                            while ($rowdb2 = mysqli_fetch_array($stmt)) {
+                                                        ?>
+                                                        <tr>
+                                                            <td><?= $rowdb2['ORDERDATE']; ?></td> <!-- TGL TERIMA ORDER -->
+                                                        </tr>
+                                                        <?php } ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<?php require_once 'footer.php'; ?>
