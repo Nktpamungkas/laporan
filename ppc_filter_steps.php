0a16830538211f59fd2fa8374eb9853a6345968c
Nktpamungkas <nktpamungkas28@gmail.com>
2024-05-03 16:09:53 +0700

    hilangin 1

--- ppc_filter_steps.php
+++ ppc_filter_steps.php
@@ -456,7 +456,7 @@
                                                                                                                 idqd.CHARACTERISTICCODE = 'GRB6' OR
                                                                                                                 idqd.CHARACTERISTICCODE = 'GRB7' OR
                                                                                                                 idqd.CHARACTERISTICCODE = 'GRB8')
-                                                                                                            AND NOT (idqd.VALUEQUANTITY = 9 OR idqd.VALUEQUANTITY = 999 OR idqd.VALUEQUANTITY = 9999 OR idqd.VALUEQUANTITY = 99999 OR idqd.VALUEQUANTITY = 99 OR idqd.VALUEQUANTITY = 91)
+                                                                                                            AND NOT (idqd.VALUEQUANTITY = 9 OR idqd.VALUEQUANTITY = 999 OR idqd.VALUEQUANTITY = 1 OR idqd.VALUEQUANTITY = 9999 OR idqd.VALUEQUANTITY = 99999 OR idqd.VALUEQUANTITY = 99 OR idqd.VALUEQUANTITY = 91)
                                                                         WHERE
                                                                             p.PRODUCTIONORDERCODE  = '$prod_order' AND p.PRODUCTIONDEMANDCODE = '$demand'  
                                                                         GROUP BY
